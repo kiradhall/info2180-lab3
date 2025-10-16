fd8154fbbe8a3899fe8f18164d3ebf4576451218
Kira Hall <kira.d.hall@gmail.com>
2025-10-16 03:25:13 -0500

    Exercise 2

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -1,7 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
   const boxes = document.querySelectorAll('#board > div')
-//let board = document.getElementById("board");
-boxes.forEach((div, i) => {
-    div.classList.add('square');})
+  let plays = []
+  let move = 'X'
   
-});
+
+  boxes.forEach((div) => {
+    div.classList.add('square');        
+
+
+    div.addEventListener('click', function(){
+      div.textContent = move;
+      plays.push(move)
+      
+      if (move  == 'X'){
+       div.classList.add('X')
+      }
+      
+      if (move == 'O'){
+       div.classList.add('O')
+      }
+
+      move = (move === 'X') ? 'O' : 'X';
+    })
+
+  });
+  
+})
